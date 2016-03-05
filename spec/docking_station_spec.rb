03ec59b995f6530f359b0eb097728b2f704842c4
Annemarie Kohler <annemarie.k89@gmail.com>
2016-03-05 15:59:15 +0000

    refactored tests, implented doubles, mocks - finished chapter 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,12 +1,12 @@
 require 'docking_station'
 
 describe DockingStation do
-   let(:bike_instance) { double :bike }
-   let(:parked_bike) { subject.park_bike(double(:bike)) }
+   let(:bike_instance) { double :bike_instance }
+   let(:parked_bike) { subject.park_bike(double(:bike_instance)) }
 
   describe "initialize" do
     it 'defaults capacity' do
-      described_class::DEFAULT_CAPACITY.times do
+      subject.capacity.times do
         subject.park_bike(bike_instance)
       end
     expect{ subject.park_bike(bike_instance)}.to raise_error 'NO MORE BIKES PLEASE'
@@ -29,19 +29,19 @@ describe DockingStation do
     it { is_expected.to respond_to(:release_bike)  }
 
     it "raises an error when there are no bikes in the docking station" do
-       expect { subject.release_bike }.to raise_error "NO BIKES"
+     expect { subject.release_bike }.to raise_error "NO BIKES"
     end
 
     it 'expects released bike to be working' do
-        subject.park_bike(bike_instance)
-          allow(bike_instance).to receive(:working).and_return(true)
-        expect(subject.release_bike.working).to eq true
+      subject.park_bike double(:bike_instance, working: true)
+      bike_instance = subject.release_bike.working
+      expect(bike_instance).to eq true
     end
 
     it 'raises an error when trying to release broken bike' do
-        subject.park_bike(bike_instance)
-          allow(bike_instance).to receive(:working).and_return(false)
-        expect { subject.release_bike }.to raise_error "BIKE IS BROKEN"
+      bike_instance = double(:bike_instance, working: false)
+      subject.park_bike(bike_instance)
+      expect { subject.release_bike }.to raise_error "BIKE IS BROKEN"
     end
   end
 
@@ -55,7 +55,7 @@ describe DockingStation do
     end
 
     it 'raises an error "NO MORE BIKES PLEASE" when there already are 20 bikes parked' do
-      DockingStation::DEFAULT_CAPACITY.times {subject.park_bike Bike.new}
+      subject.capacity.times {subject.park_bike bike_instance}
       expect { parked_bike }.to raise_error "NO MORE BIKES PLEASE"
     end
 
